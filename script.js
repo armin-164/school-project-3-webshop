487686d5b2395c6d913f897cd07067506cd71dc3
Armin Sadr <arminsadr164@hotmail.com>
2023-12-10 14:30:15 +0100

    Fix the order of all functions
    Fix the order of all functions so that are first defined and then
    being used.

--- script.js
+++ script.js
@@ -22,8 +22,24 @@ function priceIncreaseRule() {
   }
 }
 
-//Execute priceIncreaseRule when DOM content has loaded. 
+// Execute priceIncreaseRule when DOM content has loaded. 
 document.addEventListener("DOMContentLoaded", priceIncreaseRule());
+
+function returnTotalRating(element) {
+  let checkedStars = 0;
+  let halfStars = 0;
+
+  element.forEach((star) => {
+    if (star.classList.contains('checked')) {
+      checkedStars++;
+    } else if (star.classList.contains('fa-star-half-o')) {
+      halfStars++;
+    }
+  });
+
+  const totalRating = checkedStars + 0.5 * halfStars;
+  return totalRating;
+}
 
 
 // This function will sort an array alphabetically
@@ -143,28 +159,21 @@ filterListCategories.addEventListener('change', () => {
 });
 
 
-addToCartButtons.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const isItemInCartArray = isItemInArray(btn);
-
-    if (!isItemInCartArray) {
-      resetCart();
-      fetchItemInfo(btn);
-      updateCartDOM();
-    }
-    
-  });
-});
 
 function isItemInArray(e) {
-    const productName = e.closest('.product').querySelector('h3').innerText;
-    
-    for (let i = 0; i < cartArray.length; i++) {
-        if (cartArray[i].name === productName) {
-            return true;
-        }
-    }
-    return false;
+  const productName = e.closest('.product').querySelector('h3').innerText;
+  
+  for (let i = 0; i < cartArray.length; i++) {
+      if (cartArray[i].name === productName) {
+          return true;
+      }
+  }
+  return false;
+};
+
+function resetCart() {
+  const allCartItems = document.querySelectorAll('.cart-item');
+  allCartItems.forEach((item) => item.remove());
 }
 
 function fetchItemInfo(e) {
@@ -189,85 +198,126 @@ function fetchItemInfo(e) {
 
   
   cartArray.push(cartItem);
-}
-
-function returnTotalRating(element) {
-  let checkedStars = 0;
-  let halfStars = 0;
-
-  element.forEach((star) => {
-    if (star.classList.contains('checked')) {
-      checkedStars++;
-    } else if (star.classList.contains('fa-star-half-o')) {
-      halfStars++;
-    }
-  });
-
-  const totalRating = checkedStars + 0.5 * halfStars;
-  return totalRating;
-}
-
-function resetCart() {
-  const allCartItems = document.querySelectorAll('.cart-item');
-  allCartItems.forEach((item) => item.remove());
-}
-
-function updateCartDOM() {
-    cartArray.forEach((obj) => {
-    const cartItem = document.createElement('div');
-    cartItem.classList.add('cart-item');
-
-    cartItem.appendChild(
-      createItemLeftSection(obj.img, obj.name, obj.category),
-    );
-
-    cartItem.appendChild(createItemRightSection(obj.price, obj.rating, obj.amount));
-
-    updateTotalPrice();
-
-    shoppingCart.appendChild(cartItem);
-  });
-}
+};
 
 function updateTotalPrice() {
-    const cartSumDOM = document.querySelector('.cart-total-sum');
-    let cartSum = 0;
-    let totalDonuts = 0;
-    let deliveryFee = 25;
+  const cartSumDOM = document.querySelector('.cart-total-sum');
+  let cartSum = 0;
+  let totalDonuts = 0;
+  let deliveryFee = 25;
 
-    const currentDate = new Date();
-    const weekDay = currentDate.getDay();
-    const hours = currentDate.getHours();
+  const currentDate = new Date();
+  const weekDay = currentDate.getDay();
+  const hours = currentDate.getHours();
 
-    if (cartArray.length < 1) {
-      cartSumDOM.innerText = 'Your Total is:';
+  if (cartArray.length < 1) {
+    cartSumDOM.innerText = 'Your Total is:';
+  }
+
+    else {
+      cartArray.forEach((item) => {
+        cartSum += item.price;
+        totalDonuts += item.amount;
+      });
+
+      if (totalDonuts > 15) {
+        deliveryFee = 0;
+      }
+      else {
+        deliveryFee += Math.round(cartSum * 0.1);
+      }
+      if (weekDay === 1 && hours < 10) {
+        cartSumDOM.innerText = `Monday special 10%: The prices are ${Math.round(cartSum * 0.9) * 100 / 100}kr + ${deliveryFee}kr in delivery fee. Total: ${Math.round((cartSum * 0.9) + deliveryFee)}`;
+      }
+      else {
+        cartSumDOM.innerText = `The prices are ${cartSum}kr + ${deliveryFee}kr in delivery fee. Total: ${Math.round(cartSum + deliveryFee)}`;
+      }
+      if (cartSum + deliveryFee > 800) {
+        const paymentSelector = document.querySelector(".payment-method");
+        paymentSelector.removeChild(paymentSelector.options[2]);
+      }
     }
 
-      else {
-        cartArray.forEach((item) => {
-          cartSum += item.price;
-          totalDonuts += item.amount;
-        });
+};
 
-        if (totalDonuts > 15) {
-          deliveryFee = 0;
-        }
-        else {
-          deliveryFee += Math.round(cartSum * 0.1);
-        }
-        if (weekDay === 1 && hours < 10) {
-          cartSumDOM.innerText = `Monday special 10%: The prices are ${Math.round(cartSum * 0.9) * 100 / 100}kr + ${deliveryFee}kr in delivery fee. Total: ${Math.round((cartSum * 0.9) + deliveryFee)}`;
-        }
-        else {
-          cartSumDOM.innerText = `The prices are ${cartSum}kr + ${deliveryFee}kr in delivery fee. Total: ${Math.round(cartSum + deliveryFee)}`;
-        }
-        if (cartSum + deliveryFee > 800) {
-          const paymentSelector = document.querySelector(".payment-method");
-          paymentSelector.removeChild(paymentSelector.options[2]);
-        }
+function increaseOrDecrease(e, symbol, element) {
+  const cartItem = e.target.closest('.cart-item');
+  const cartItemName = cartItem.querySelector('h4').innerText;
+  const itemPriceDOM = e.target.closest('.item-right-section').querySelector('.item-total-price');
+  let defaultProductPrice;
+
+  const productsDOM = document.querySelectorAll('.product');
+  productsDOM.forEach((product) => {
+    const productName = product.querySelector("h3").innerText;
+    if (productName === cartItemName) {
+       defaultProductPrice = Math.round(parseFloat(product.querySelector('.product-price').innerText, 10) * 100) / 100;
+    }
+  });
+
+  cartArray.forEach((obj) => {
+
+      if (cartItemName === obj.name && obj.amount >= 1 && symbol === "+") {
+          obj.amount += 1;
+          element.value = obj.amount;
+          obj.price = defaultProductPrice * obj.amount;
+      
+
+          if (obj.amount >= 10) {
+            obj.price *= 0.9;
+          }
+
+          itemPriceDOM.innerText = `${obj.price}kr`;
+
       }
 
-}
+      else if (cartItemName === obj.name && obj.amount >=2  && symbol === "-") {
+          obj.amount -= 1;
+          element.value = obj.amount;
+          obj.price = defaultProductPrice * obj.amount;
+
+          if (obj.amount >= 10) {
+            obj.price *= 0.9;
+          }
+          
+          itemPriceDOM.innerText = `${obj.price}kr`;
+      }    
+  })
+};
+
+function removeFromCartArray (e) {
+  const productName = e.closest('.cart-item').querySelector("h4").innerText;
+  const itemIndex = cartArray.findIndex(obj => obj.name === productName);
+  cartArray.splice(itemIndex, 1);
+};
+
+
+function calculate_And_Return_Rating(rating) {
+  const itemRatingContainer = document.createElement('div');
+  itemRatingContainer.classList.add('rating');
+
+  const checkedStars = Math.floor(rating);
+  const halfStars = rating % 1 === 0.5;
+  const emptyStars = 5 - Math.round(rating);
+
+  for (let i = 0; i < checkedStars; i++) {
+    const starSpanElement = document.createElement('span');
+    starSpanElement.classList.add('fa', 'fa-star', 'checked');
+    itemRatingContainer.appendChild(starSpanElement);
+  }
+
+  if (halfStars) {
+    const halfStarSpanElement = document.createElement('span');
+    halfStarSpanElement.classList.add('fa', 'fa-star-half-o');
+    itemRatingContainer.appendChild(halfStarSpanElement);
+  }
+
+  for (let i = 0; i < emptyStars; i++) {
+    const emptyStarSpanElement = document.createElement('span');
+    emptyStarSpanElement.classList.add('fa', 'fa-star-half-o');
+    itemRatingContainer.appendChild(emptyStarSpanElement);
+  }
+  return itemRatingContainer;
+};
 
 function createItemLeftSection(img, name, category) {
   const itemLeftSection = document.createElement('div');
@@ -291,7 +341,8 @@ function createItemLeftSection(img, name, category) {
   itemLeftSection.appendChild(itemInfo);
 
   return itemLeftSection;
-}
+};
+
 
 function createItemRightSection(price, rating, amount) {
   const itemRightSection = document.createElement('div');
@@ -330,7 +381,7 @@ function createItemRightSection(price, rating, amount) {
   increaseBtn.addEventListener("click", (e) => {
     increaseOrDecrease(e, "+", itemAmountInput);
     updateTotalPrice();
-  })
+  });
 
 
   itemPriceContainer.appendChild(itemPrice);
@@ -350,87 +401,42 @@ function createItemRightSection(price, rating, amount) {
   itemRightSection.appendChild(removeText);
 
   return itemRightSection;
+};
+
+
+
+
+function updateCartDOM() {
+  cartArray.forEach((obj) => {
+  const cartItem = document.createElement('div');
+  cartItem.classList.add('cart-item');
+
+  cartItem.appendChild(
+    createItemLeftSection(obj.img, obj.name, obj.category),
+  );
+
+  cartItem.appendChild(createItemRightSection(obj.price, obj.rating, obj.amount));
+
+  updateTotalPrice();
+
+  shoppingCart.appendChild(cartItem);
+});
 }
 
-function increaseOrDecrease(e, symbol, element) {
-    const cartItem = e.target.closest('.cart-item');
-    const cartItemName = cartItem.querySelector('h4').innerText;
-    const itemPriceDOM = e.target.closest('.item-right-section').querySelector('.item-total-price');
-    let defaultProductPrice;
+addToCartButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const isItemInCartArray = isItemInArray(btn);
 
-    const productsDOM = document.querySelectorAll('.product');
-    productsDOM.forEach((product) => {
-      const productName = product.querySelector("h3").innerText;
-      if (productName === cartItemName) {
-         defaultProductPrice = Math.round(parseFloat(product.querySelector('.product-price').innerText, 10) * 100) / 100;
-      }
-    });
-
-    cartArray.forEach((obj) => {
-
-        if (cartItemName === obj.name && obj.amount >= 1 && symbol === "+") {
-            obj.amount += 1;
-            element.value = obj.amount;
-            obj.price = defaultProductPrice * obj.amount;
-        
-
-            if (obj.amount >= 10) {
-              obj.price *= 0.9;
-            }
-
-            itemPriceDOM.innerText = `${obj.price}kr`;
-
-        }
-
-        else if (cartItemName === obj.name && obj.amount >=2  && symbol === "-") {
-            obj.amount -= 1;
-            element.value = obj.amount;
-            obj.price = defaultProductPrice * obj.amount;
-
-            if (obj.amount >= 10) {
-              obj.price *= 0.9;
-            }
-            
-            itemPriceDOM.innerText = `${obj.price}kr`;
-        }    
-    })
+    if (!isItemInCartArray) {
+      resetCart();
+      fetchItemInfo(btn);
+      updateCartDOM();
+    }
+    
+  });
+});
 
 
-}
-
-function removeFromCartArray (e) {
-    const productName = e.closest('.cart-item').querySelector("h4").innerText;
-    const itemIndex = cartArray.findIndex(obj => obj.name === productName);
-    cartArray.splice(itemIndex, 1);
-}
-
-function calculate_And_Return_Rating(rating) {
-  const itemRatingContainer = document.createElement('div');
-  itemRatingContainer.classList.add('rating');
-
-  const checkedStars = Math.floor(rating);
-  const halfStars = rating % 1 === 0.5;
-  const emptyStars = 5 - Math.round(rating);
-
-  for (let i = 0; i < checkedStars; i++) {
-    const starSpanElement = document.createElement('span');
-    starSpanElement.classList.add('fa', 'fa-star', 'checked');
-    itemRatingContainer.appendChild(starSpanElement);
-  }
-
-  if (halfStars) {
-    const halfStarSpanElement = document.createElement('span');
-    halfStarSpanElement.classList.add('fa', 'fa-star-half-o');
-    itemRatingContainer.appendChild(halfStarSpanElement);
-  }
-
-  for (let i = 0; i < emptyStars; i++) {
-    const emptyStarSpanElement = document.createElement('span');
-    emptyStarSpanElement.classList.add('fa', 'fa-star-half-o');
-    itemRatingContainer.appendChild(emptyStarSpanElement);
-  }
-  return itemRatingContainer;
-}
 
 
 const submitOrderBtn = document.querySelector('.submit-btn');
@@ -446,92 +452,42 @@ function validateInputField(negate, regex, input, msg ) {
         input.setCustomValidity('');
     }
     
-}
+};
+
+function removePaymentInfo(element) {
+  const allInputs = element.querySelectorAll('input');
+  allInputs.forEach((input) => {
+      input.value = "";
+  })
+};
+
+function displayPaymentMethod(method) {
+  const invoicePaymentDiv = document.querySelector('#payment-invoice');
+  const cardPaymentDiv = document.querySelector('#payment-card');
+
+  if (method === 'invoice') {
+      removePaymentInfo(cardPaymentDiv);
+      cardPaymentDiv.style.display = "none";
+      invoicePaymentDiv.style.display = "flex";
+  }
+  else if (method === 'card') {
+      removePaymentInfo(invoicePaymentDiv);
+      invoicePaymentDiv.style.display = "none";
+      cardPaymentDiv.style.display = "flex";
+  }
+};
 
 paymentSelector.addEventListener('change', () => {
     displayPaymentMethod(paymentSelector.value);
 });
 
-function removePaymentInfo(element) {
-    const allInputs = element.querySelectorAll('input');
-    allInputs.forEach((input) => {
-        input.value = "";
-    })
-}
 
-function displayPaymentMethod(method) {
-    const invoicePaymentDiv = document.querySelector('#payment-invoice');
-    const cardPaymentDiv = document.querySelector('#payment-card');
-
-    if (method === 'invoice') {
-        removePaymentInfo(cardPaymentDiv);
-        cardPaymentDiv.style.display = "none";
-        invoicePaymentDiv.style.display = "flex";
-    }
-    else if (method === 'card') {
-        removePaymentInfo(invoicePaymentDiv);
-        invoicePaymentDiv.style.display = "none";
-        cardPaymentDiv.style.display = "flex";
-    }
-}
-
-submitOrderBtn.addEventListener("click", () => {
-    validateOrderForm();
-}); 
 
 resetOrderBtn.addEventListener("click", () => {
   resetCart();
   cartArray.splice(0, cartArray.length);  
   updateTotalPrice();
-})
-
-
-function validateOrderForm() {
-    const hasNumbersOrSymbols = /[\d!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
-    const containsFiveNumbers = /^\d{5}$/;
-    const validPhoneNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    const containsSixteenNumbers = /^\d{16}$/;
-    const isYearAndMonth = /^\d{4}\/\d{2}$/;
-    const containsThreeNumbers = /^\d{3}$/;
-    const isAnSSN = /^(\d{10}|\d{12})$/;
-
-
-
-    const userFirstAndLastName = document.querySelectorAll('.user-name');
-    userFirstAndLastName.forEach(name => validateInputField(false, hasNumbersOrSymbols, name, "Please exclude any numbers or symbols"));
-
-    const userAdress = document.querySelector('.user-adress');
-
-    const userPostalCode = document.querySelector('.user-postalcode');
-    validateInputField(true, containsFiveNumbers, userPostalCode, "Invalid Postal Code");
-
-
-    const userCity = document.querySelector('.user-city');
-    validateInputField(false, hasNumbersOrSymbols, userCity, 'Please exclude any numbers/symbols');
-    
-    const userPhone = document.querySelector('.user-phone');
-    validateInputField(true, validPhoneNumber, userPhone, 'Invalid Phone Number');
-
-    const userEmail = document.querySelector('.user-email');
-
-    const userCardNumber = document.querySelector('.user-card-number');
-    const userCardExpiryDate = document.querySelector('.user-card-expiry-date');
-    const userCardCvc = document.querySelector('.user-card-cvc');
-
-    const userSSN = document.querySelector('.user-ssn');
-
-    if (userCardNumber.value === "" && userCardExpiryDate.value === "" && userCardCvc.value === "") {
-        validateInputField(true, isAnSSN, userSSN, 'Please write your SSN with 10 or 12 digits');
-    }
-
-    else if (userSSN.value === "") {
-        validateInputField(true, containsSixteenNumbers, userCardNumber, 'Invalid Card Number')
-        validateInputField(true, isYearAndMonth, userCardExpiryDate, 'Please write in this format: YYYY/MM');
-        validateInputField(true, containsThreeNumbers, userCardCvc, 'Please write a three-digit CVC');
-    }
-
-    displayOrderConfirmation(userFirstAndLastName, userAdress, userPostalCode, userCity, userEmail, userPhone);
-}
+});
 
 function formIsValid() {
   const formRows = document.querySelectorAll('.form-row[style*="display: flex"]');
@@ -556,7 +512,7 @@ function formIsValid() {
     return true;
   }
   
-}
+};
 
 function calculateDeliveryTime() {
   const currentDate = new Date();
@@ -583,9 +539,7 @@ function calculateDeliveryTime() {
     currentDate.setMinutes(currentDate.getMinutes() + 30);  
     return `Your order will be delivered at ${currentDate.getHours()}:${currentDate.getMinutes()}`;
   }
-} 
-
-
+};
 
 function displayOrderConfirmation (names, adress, postalCode, city, email, phone) {
   const sectionCheckout = document.querySelector('.section-checkout');
@@ -663,5 +617,57 @@ function displayOrderConfirmation (names, adress, postalCode, city, email, phone
   else if (sectionCheckout.querySelector('.order-confirmation')) {
     alert("Please update your page!");
    }
-}
+};
+
+
+function validateOrderForm() {
+    const hasNumbersOrSymbols = /[\d!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+    const containsFiveNumbers = /^\d{5}$/;
+    const validPhoneNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    const containsSixteenNumbers = /^\d{16}$/;
+    const isYearAndMonth = /^\d{4}\/\d{2}$/;
+    const containsThreeNumbers = /^\d{3}$/;
+    const isAnSSN = /^(\d{10}|\d{12})$/;
+
+
+
+    const userFirstAndLastName = document.querySelectorAll('.user-name');
+    userFirstAndLastName.forEach(name => validateInputField(false, hasNumbersOrSymbols, name, "Please exclude any numbers or symbols"));
+
+    const userAdress = document.querySelector('.user-adress');
+
+    const userPostalCode = document.querySelector('.user-postalcode');
+    validateInputField(true, containsFiveNumbers, userPostalCode, "Invalid Postal Code");
+
+
+    const userCity = document.querySelector('.user-city');
+    validateInputField(false, hasNumbersOrSymbols, userCity, 'Please exclude any numbers/symbols');
+    
+    const userPhone = document.querySelector('.user-phone');
+    validateInputField(true, validPhoneNumber, userPhone, 'Invalid Phone Number');
+
+    const userEmail = document.querySelector('.user-email');
+
+    const userCardNumber = document.querySelector('.user-card-number');
+    const userCardExpiryDate = document.querySelector('.user-card-expiry-date');
+    const userCardCvc = document.querySelector('.user-card-cvc');
+
+    const userSSN = document.querySelector('.user-ssn');
+
+    if (userCardNumber.value === "" && userCardExpiryDate.value === "" && userCardCvc.value === "") {
+        validateInputField(true, isAnSSN, userSSN, 'Please write your SSN with 10 or 12 digits');
+    }
+
+    else if (userSSN.value === "") {
+        validateInputField(true, containsSixteenNumbers, userCardNumber, 'Invalid Card Number')
+        validateInputField(true, isYearAndMonth, userCardExpiryDate, 'Please write in this format: YYYY/MM');
+        validateInputField(true, containsThreeNumbers, userCardCvc, 'Please write a three-digit CVC');
+    }
+
+    displayOrderConfirmation(userFirstAndLastName, userAdress, userPostalCode, userCity, userEmail, userPhone);
+};
+
+submitOrderBtn.addEventListener("click", () => {
+  validateOrderForm();
+}); 
 
